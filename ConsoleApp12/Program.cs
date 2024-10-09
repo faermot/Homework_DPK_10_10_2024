099df768ed2975d8bf09dde91c2a3715fb82ab59
Ilya Karasev <97747062+faermot@users.noreply.github.com>
2024-10-09 20:17:48 +0300

    Update

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Task1()
+        {
+            Console.WriteLine("Задание №1");
+
+            Random rnd = new Random();
+            int[,] array = new int[3, 4];
+            int[] temp_array = new int[array.GetLength(1)];
+
+            Console.WriteLine("\nИсходный массив: ");
+            for (int i = 0; i < array.GetLength(0); i++) 
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(-50, 51);
+                    if (i == 2)
+                    {
+                        temp_array[j] = array[i, j];
+                    }
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine();
+            }
+
+            Array.Sort(temp_array);
+
+            Console.WriteLine("\nИтоговый массив: ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == 2)
+                    {
+                        array[i, j] = temp_array[j];
+                    }
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine("Задание №2");
+
+            Random rnd = new Random();
+            int[,] array = new int[7, 7];
+            int sum = 0;
+
+            Console.WriteLine("\nИсходный массив: ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(-50, 51);
+                    if (array[i, j] % 2 != 0 && array[i, j] < 0)
+                    {
+                        sum += Math.Abs(array[i, j]);
+                    }
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"\nСумма модулей отрицательных нечётных элементов данного масива: {sum}");
+
+            Console.ReadKey();
+
+        }
+
+        static void Task3()
+        {
+            Console.WriteLine("Задание №3");
+
+            Random rnd = new Random();
+            int[,] array = new int[5, 6];
+            int sum = 0;
+
+            Console.WriteLine("\nИсходный массив: ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(-50, 51);
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nСреднее арифметическое элементов столбца:");
+
+            for (int i = 0; i < array.GetLength(1); i++)
+            {
+                for (int j = 0; j < array.GetLength(0); j++)
+                {
+                    sum += array[j, i];
+                }
+                Console.WriteLine($"{i + 1}. {sum / array.GetLength(1)}");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Task4()
+        {
+            Console.WriteLine("Задание №4");
+
+            Random rnd = new Random();
+            int[,] array = new int[5, 5];
+            int[] diagonal = new int[5];
+            int x = 52, count = 4;
+
+            Console.WriteLine("\nИсходный массив: ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(-50, 51);
+                    Console.Write($" {array[i, j]}");
+                    if (j == count)
+                    {
+                        count--;
+                        if (array[i, j] < x) x = array[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"\nНаименьший элемент на побочной диагонали: {x}");
+
+            Console.ReadKey();
+        }
+
+        static void Task5()
+        {
+            Console.WriteLine("Задание №1");
+
+            Random rnd = new Random();
+            int[,] array = new int[5, 4];
+            int[] temp_array = new int[array.GetLength(0)];
+
+            Console.WriteLine("\nИсходный массив: ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = rnd.Next(-50, 51);
+                    if (j == 3)
+                    {
+                        temp_array[i] = array[i, j];
+                    }
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine();
+            }
+
+            Array.Sort(temp_array);
+
+            Console.WriteLine("\nИтоговый массив: ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (j == 3)
+                    {
+                        array[i, j] = temp_array[i];
+                    }
+                    Console.Write($" {array[i, j]}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.Clear();
+                Console.Write("Выберите задание (1-5): ");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Task1();
+                        break;
+
+                    case "2":
+                        Task2();
+                        break;
+
+                    case "3":
+                        Task3();
+                        break;
+
+                    case "4":
+                        Task4();
+                        break;
+
+                    case "5":
+                        Task5();
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("\aВыберите корректное задание!");
+                        Thread.Sleep(2000);
+                        break;
+                }
+            }
+        }
+    }
+}
